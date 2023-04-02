39469c5bfa504215be804d4939804f6236534a5e
Paulo Augusto <paulo-augusto12@outlook.com>
2023-04-01 23:01:47 -0300

    feat: added initial style for development

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -2,14 +2,29 @@ import React from "react";
 import { Container } from "./Components/container";
 import { useTheme } from "./hook/useTheme";
 import { Typography } from "./Components/Typography";
+import { Box } from "./Components/Box";
 
 export function HomePage() {
   const theme = useTheme();
   return (
     <Container>
-      <Typography sx={{ color: theme.colors.fontColor }}>
-        Hello world
-      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          paddingTop: "9.6875rem",
+        }}
+      >
+        <Typography
+          sx={{
+            color: theme.colors.fontColor,
+            fontWeight: theme.weigths.bold,
+            fontSize: theme.fonts.large,
+          }}
+        >
+          Hello world
+        </Typography>
+      </Box>
     </Container>
   );
 }
