f32be751509d2499af1b32f2226e91c73cbb916b
Paulo Augusto <paulo-augusto12@outlook.com>
2023-04-01 22:33:38 -0300

    feat: started home page development

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { HomePage } from "./HomePage";
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <HomePage />
     </div>
   );
 }
---
+++ src/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function HomePage() {
+  return (
+    <div>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </div>
+  );
+}
