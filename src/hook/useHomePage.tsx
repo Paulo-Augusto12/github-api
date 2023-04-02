de5e9d587a6582f05fe1d525b126b1f4893dd39b
Paulo Augusto <paulo-augusto12@outlook.com>
2023-04-02 14:07:15 -0300

    feat: added functions to hook

--- src/hook/useHomePage.tsx
+++ src/hook/useHomePage.tsx
@@ -1,3 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { IUserDTO } from "../api/DTO/userDTO";
+import { IUser } from "../api/InterfaceMapper/IUser";
+import { GetUserdata } from "../api/getUserData";
 
-export function useHomePage() {}
+export function useHomePage() {
+  const [userFound, setUserFound] = useState<IUser>();
+  const [usernameInput, setUsernameInput] = useState("");
+  function userDTOToUserState(DTO: IUserDTO): IUser {
+    return {
+      profileImage: DTO.avatar_url,
+      name: DTO.name,
+      githubLink: DTO.html_url,
+      description: DTO.bio,
+      followers: DTO.followers,
+      following: DTO.following,
+      repos: DTO.public_repos,
+    };
+  }
+
+  async function handleGetuserData() {
+    const response = await GetUserdata(usernameInput);
+
+    const data = userDTOToUserState(response);
+
+    setUserFound(data);
+
+    console.log(userFound, "user");
+  }
+
+  return {
+    userFound,
+    usernameInput,
+    setUsernameInput,
+    handleGetuserData,
+  };
+}
