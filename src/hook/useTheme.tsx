1ccefbbe35d79c7aed03e1a18e6d8017886eb599
Paulo Augusto <paulo-augusto12@outlook.com>
2023-04-01 22:51:17 -0300

    feat: created hook for common styles that are going to be used during development

---
+++ src/hook/useTheme.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export function useTheme() {
+  const colors = {
+    backgroundColor: "#0077B6",
+    cardColor: "#116694",
+    fontColor: "#A8DADC",
+    linkColor: "#457B9D",
+  };
+
+  const fonts = {
+    small: 12,
+    medium: 16,
+    large: 32,
+  };
+
+  const weigths = {
+    medium: 600,
+    bold: 800,
+  };
+
+  return {
+    fonts,
+    colors,
+    weigths,
+  };
+}
